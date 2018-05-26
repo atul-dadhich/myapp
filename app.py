8f76118131eaf5024ca928b31deca8dfc7445339
Atul Dadhich <atul.dadhich@actian.com>
2018-05-27 00:58:44 +0530

    Added a get call to fetch current time and date.

---
+++ app.py
@@ -0,0 +1,12 @@
+import datetime
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/getdate")
+def date_finder():
+    return "Current Date and Time is " + str(datetime.datetime.now())
+
+if __name__ == '__main__':
+    app.run(debug=True)
