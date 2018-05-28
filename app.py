a1449352fc0d95b21aa4b07a11d235255d819baa
Atul Dadhich <atul.dadhich@actian.com>
2018-05-28 17:29:54 +0530

    Changed the Prompt message to check jenkins build trigger.

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/getdate")
 def date_finder():
-    return "Current Date and Time is " + str(datetime.datetime.now())
+    return "Latest Current Date and Time is " + str(datetime.datetime.now())
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
