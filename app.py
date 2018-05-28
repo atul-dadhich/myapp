b9605c5029002d880db03e6235ea1a3e8a6d68a3
Atul Dadhich <atul.dadhich@actian.com>
2018-05-28 18:06:02 +0530

    Changed Prompt and getdate is back.

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 
 @app.route("/getdate")
 def date_finder():
-    return "I have completed my first novice kind of code pipeline"
+    return "I have completed my first novice kind of code pipeline, Today's Date is " + str(datetime.datetime.now())
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
