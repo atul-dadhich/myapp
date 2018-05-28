ef5cc8c36f0fd9ff30ebecd860f65d0e4a927233
Atul Dadhich <atul.dadhich@actian.com>
2018-05-28 17:57:31 +0530

    new build with changed return message

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/getdate")
 def date_finder():
-    return "Second Latest Current Date and Time is " + str(datetime.datetime.now())
+    return "I have completed my first novice kind of code pipeline"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
